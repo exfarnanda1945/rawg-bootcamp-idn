6cd60687e173d5424895b0d4c787d2518ae1c193
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-01-20 14:55:57 +0700

    add: constant variable

---
+++ app/src/main/java/com/example/rawgbootcampidn/utils/Constant.kt
@@ -0,0 +1,10 @@
+package com.example.rawgbootcampidn.utils
+
+object Constant {
+    // Retrofit
+    const val BASE_URL ="https://api.rawg.io/api/"
+    const val API_KEY ="47b9f7a441b54351ae6a87a3809ea25a"
+    const val QUERY_PAGE_SIZE = "page_size"
+    const val QUERY_ORDERING ="ordering"
+    const val QUERY_DATES="dates"
+}

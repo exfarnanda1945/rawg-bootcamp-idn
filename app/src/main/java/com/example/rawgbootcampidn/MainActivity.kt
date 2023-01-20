ec201461b2deb021584127e515c98cf09b477a76
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-01-20 14:52:25 +0700

    add: observe game list at main activity

--- app/src/main/java/com/example/rawgbootcampidn/MainActivity.kt
+++ app/src/main/java/com/example/rawgbootcampidn/MainActivity.kt
@@ -1,11 +1,87 @@
 package com.example.rawgbootcampidn
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.rawgbootcampidn.adapter.GameAdapter
+import com.example.rawgbootcampidn.data.network.handler.NetworkResult
+import com.example.rawgbootcampidn.databinding.ActivityMainBinding
+import com.example.rawgbootcampidn.model.GameResult
+import com.example.rawgbootcampidn.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val mainViewModel by viewModels<MainViewModel>()
+    private val gameAdapter by lazy { GameAdapter() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        mainViewModel.listGame.observe(this) { result ->
+            when (result) {
+                is NetworkResult.Loading -> {
+                    handleUi(
+                        recyclerView = false,
+                        progressBar = true,
+                        errorTv = false,
+                        errorImg = false
+                    )
+                }
+                is NetworkResult.Error -> {
+                    binding.errorText.text = result.errorMessage
+                    handleUi(
+                        recyclerView = false,
+                        progressBar = false,
+                        errorTv = true,
+                        errorImg = true
+                    )
+                }
+                is NetworkResult.Success -> {
+                    binding.rvBestGame.apply {
+                        adapter = gameAdapter
+                        setHasFixedSize(true)
+                        layoutManager = LinearLayoutManager(
+                            this@MainActivity,
+                            LinearLayoutManager.VERTICAL,
+                            false
+                        )
+                        gameAdapter.setData(result.data?.results)
+                    }
+                    gameAdapter.setOnItemClickCallback(object : GameAdapter.IOnItemCallBack {
+                        override fun onItemClickCallback(data: GameResult) {
+                            Log.d("nah", "bruh")
+                        }
+
+                    })
+                    handleUi(
+                        recyclerView = true,
+                        progressBar = false,
+                        errorTv = false,
+                        errorImg = false
+                    )
+                }
+            }
+        }
+    }
+
+
+    private fun handleUi(
+        recyclerView: Boolean,
+        progressBar: Boolean,
+        errorTv: Boolean,
+        errorImg: Boolean
+    ) {
+        binding.apply {
+            rvBestGame.isVisible = recyclerView
+            progressbar.isVisible = progressBar
+            errorIcon.isVisible = errorImg
+            errorText.isVisible = errorTv
+        }
     }
 }

33c741cfb3e19e5c2a53eea73a5e6b6391a230f5
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-01-22 10:10:27 +0700

    add: game detail and screenshot api

--- app/src/main/java/com/example/rawgbootcampidn/data/network/api/GameApi.kt
+++ app/src/main/java/com/example/rawgbootcampidn/data/network/api/GameApi.kt
@@ -1,8 +1,11 @@
 package com.example.rawgbootcampidn.data.network.api
 
 import com.example.rawgbootcampidn.model.Game
+import com.example.rawgbootcampidn.model.GameDetail
+import com.example.rawgbootcampidn.model.Screenshots
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.QueryMap
 
 interface GameApi {
@@ -10,4 +13,15 @@ interface GameApi {
     suspend fun getGameList(
         @QueryMap queries: Map<String, String>
     ): Response<Game>
+
+    @GET("games/{gameId}")
+    suspend fun getGameDetailById(
+        @Path("gameId") gameId:Int
+    ):Response<GameDetail>
+
+    @GET("games/{gameId}/screenshots")
+    suspend fun getGameScreenshots(
+        @Path("gameId") gameId: Int
+    ):Response<Screenshots>
+
 }

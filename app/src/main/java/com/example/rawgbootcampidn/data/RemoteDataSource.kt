006f724cc888a899249e6fe251ad73eaaf471ea6
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-01-22 10:04:09 +0700

    fix: get game api remote source

--- app/src/main/java/com/example/rawgbootcampidn/data/RemoteDataSource.kt
+++ app/src/main/java/com/example/rawgbootcampidn/data/RemoteDataSource.kt
@@ -4,8 +4,12 @@ import android.util.Log
 import com.example.rawgbootcampidn.data.network.api.GameApi
 import com.example.rawgbootcampidn.data.network.handler.NetworkResult
 import com.example.rawgbootcampidn.model.Game
+import com.example.rawgbootcampidn.model.GameDetail
+import com.example.rawgbootcampidn.model.Screenshots
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 class RemoteDataSource(private val gameApi: GameApi) {
     suspend fun getGameList(queries: Map<String, String>): Flow<NetworkResult<Game>> = flow {
@@ -29,7 +33,61 @@ class RemoteDataSource(private val gameApi: GameApi) {
             }
         } catch (e: Exception) {
             e.printStackTrace()
-            Log.d("dataSourceError", "${e.message}")
+            Log.d("remoteError", "${e.message}")
+            emit(NetworkResult.Error("Something went wrong. Please check log."))
+        }
+    }.flowOn(Dispatchers.IO)
+
+    suspend fun getGameDetailById(gameId: Int): Flow<NetworkResult<GameDetail>> = flow {
+        try {
+            emit(NetworkResult.Loading(true))
+            val gameDetail = gameApi.getGameDetailById(gameId)
+            if (gameDetail.isSuccessful) {
+                val responseBody = gameDetail.body()
+
+                if (responseBody != null) {
+                    emit(NetworkResult.Success(responseBody))
+                } else {
+                    emit(NetworkResult.Error("Can't fetch detail game."))
+                }
+            } else {
+                // request data failed
+                Log.d(
+                    "apiServiceError",
+                    "statusCode:${gameDetail.code()}, message:${gameDetail.message()}"
+                )
+                emit(NetworkResult.Error("Failed to fetch data from server."))
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.d("remoteError", "${e.message}")
+            emit(NetworkResult.Error("Something went wrong. Please check log."))
+        }
+    }.flowOn(Dispatchers.IO)
+
+    suspend fun getGameScreenshotById(gameId: Int): Flow<NetworkResult<Screenshots>> = flow {
+        try {
+            emit(NetworkResult.Loading(true))
+            val screenshots = gameApi.getGameScreenshots(gameId)
+
+            if (screenshots.isSuccessful) {
+                val responseBody = screenshots.body()
+                if (responseBody?.results?.isEmpty() == true) {
+                    emit(NetworkResult.Error("Screenshots game not found"))
+                } else {
+                    emit(NetworkResult.Success(responseBody!!))
+                }
+            } else {
+                Log.d(
+                    "apiServiceError",
+                    "statusCode:${screenshots.code()}, message:${screenshots.message()}"
+                )
+                emit(NetworkResult.Error("Failed to fetch data from server."))
+            }
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.d("remoteError", "${e.message}")
             emit(NetworkResult.Error("Something went wrong. Please check log."))
         }
     }

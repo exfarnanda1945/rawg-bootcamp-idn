4cf180a8701361a6f79fc85b33e0022203d54557
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-01-26 13:30:24 +0700

    add: is favorite game in detail activity

--- app/src/main/java/com/example/rawgbootcampidn/DetailActivity.kt
+++ app/src/main/java/com/example/rawgbootcampidn/DetailActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.rawgbootcampidn.adapter.ScreenshotGameAdapter
+import com.example.rawgbootcampidn.data.database.GameEntity
 import com.example.rawgbootcampidn.data.network.handler.NetworkResult
 import com.example.rawgbootcampidn.databinding.ActivityDetailBinding
+import com.example.rawgbootcampidn.model.GameDetail
 import com.example.rawgbootcampidn.model.GameResult
 import com.example.rawgbootcampidn.viewmodels.DetailViewModel
 
@@ -16,21 +19,24 @@ class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
     private val detailViewModel by viewModels<DetailViewModel>()
     private val screenshotAdapter by lazy { ScreenshotGameAdapter() }
+    private lateinit var gameDetail: GameDetail
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
+        supportActionBar?.apply {
+            title = "Detail Game"
+            setDisplayHomeAsUpEnabled(true)
+        }
         val game = intent.getParcelableExtra<GameResult>(EXTRA_GAME)
 
         detailViewModel.fetchGameDetail(game?.id!!)
         detailViewModel.fetchScreenshotsGame(game.id)
 
 
-        detailViewModel.screenshotsGame.observe(this){result ->
-            if(result is NetworkResult.Success){
+        detailViewModel.screenshotsGame.observe(this) { result ->
+            if (result is NetworkResult.Success) {
                 binding.screenshotsGame.apply {
                     adapter = screenshotAdapter
                     setHasFixedSize(true)
@@ -62,6 +68,7 @@ class DetailActivity : AppCompatActivity() {
                 }
                 is NetworkResult.Success -> {
                     binding.gameDetail = result.data
+                    gameDetail = result.data!!
                     handleUi(
                         layoutWrapper = true,
                         progressBar = false,
@@ -73,6 +80,54 @@ class DetailActivity : AppCompatActivity() {
         }
 
 
+        isFavoriteGame(game)
+    }
+
+    private fun isFavoriteGame(gameSelected: GameResult) {
+        detailViewModel.favoriteGameList.observe(this) { result ->
+            val game = result.find { favorite ->
+                favorite.game.id == gameSelected.id
+            }
+            if (game != null) {
+                binding.addFavoriteBtn.apply {
+                    setText(R.string.remove_from_favorite)
+                    setBackgroundColor(
+                        ContextCompat.getColor(
+                            this@DetailActivity,
+                            R.color.red_star
+                        )
+                    )
+                    setOnClickListener {
+                        deleteFavoriteGame(game.id)
+                    }
+                }
+            } else {
+                binding.addFavoriteBtn.apply {
+                    setText(R.string.add_to_favorite)
+                    setBackgroundColor(
+                        ContextCompat.getColor(
+                            this@DetailActivity,
+                            R.color.light_blue
+                        )
+                    )
+                    setOnClickListener {
+                        insertFavoriteGame()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun deleteFavoriteGame(gameEntityId: Int) {
+        val gameEntity = GameEntity(gameEntityId,gameDetail)
+        detailViewModel.deleteFavoriteGame(gameEntity)
+        Toast.makeText(this, "Successfully remove from favorite", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun insertFavoriteGame() {
+        val gameEntity = GameEntity(game = gameDetail)
+        detailViewModel.insertFavoriteGame(gameEntity)
+        Toast.makeText(this, "Successfully added to favorite", Toast.LENGTH_SHORT).show()
     }
 
     private fun handleUi(

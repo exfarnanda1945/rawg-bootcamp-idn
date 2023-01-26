fdad321b43b496f16311493ee9cf049769326ba9
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-01-26 13:18:51 +0700

    fix: Game entity and type converter result type

--- app/src/main/java/com/example/rawgbootcampidn/data/database/GameEntity.kt
+++ app/src/main/java/com/example/rawgbootcampidn/data/database/GameEntity.kt
@@ -1,13 +1,16 @@
 package com.example.rawgbootcampidn.data.database
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.example.rawgbootcampidn.model.GameResult
+import com.example.rawgbootcampidn.model.GameDetail
 import com.example.rawgbootcampidn.utils.Constant.GAME_TABLE_NAME
+import kotlinx.parcelize.Parcelize
 
 @Entity(tableName = GAME_TABLE_NAME)
+@Parcelize
 data class GameEntity(
     @PrimaryKey(autoGenerate = true)
-    val id: Int,
-    val game: GameResult
-)
+    val id: Int = 0,
+    val game: GameDetail
+):Parcelable
--- app/src/main/java/com/example/rawgbootcampidn/data/database/GameTypeConverter.kt
+++ app/src/main/java/com/example/rawgbootcampidn/data/database/GameTypeConverter.kt
@@ -1,7 +1,7 @@
 package com.example.rawgbootcampidn.data.database
 
 import androidx.room.TypeConverter
-import com.example.rawgbootcampidn.model.GameResult
+import com.example.rawgbootcampidn.model.GameDetail
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
@@ -9,13 +9,13 @@ class GameTypeConverter {
     private val gson = Gson()
 
     @TypeConverter
-    fun gameDataToString(game:GameResult): String {
+    fun gameDataToString(game:GameDetail): String {
         return gson.toJson(game)
     }
 
     @TypeConverter
-    fun gameStringToData(string: String): GameResult {
-        val listType = object : TypeToken<GameResult>() {}.type
+    fun gameStringToData(string: String): GameDetail {
+        val listType = object : TypeToken<GameDetail>() {}.type
         return gson.fromJson(string, listType)
     }
 }
